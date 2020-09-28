f3671b54b748d3b069aca844e2450ad8845ca0c9
Charlie Baez <cbaez@horizontal.com>
2020-09-28 08:54:01 -0500

    HTTPs update

--- js/forecast.js
+++ js/forecast.js
@@ -1,7 +1,7 @@
 const key = 'Zit35EuVaJ9q77GnyAHlOnGdHZlUpSJD';
 //get weather
 const getWeather = async id => {
-	const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+	const base = 'https://dataservice.accuweather.com/currentconditions/v1/';
 	const query = `${id}?apikey=${key}`;
 
 	const response = await fetch(base + query);
@@ -10,7 +10,7 @@ const getWeather = async id => {
 };
 //get City
 const getCity = async city => {
-	const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+	const base = 'https://dataservice.accuweather.com/locations/v1/cities/search';
 	const query = `?apikey=${key}&q=${city}`;
 
 	const response = await fetch(base + query);
